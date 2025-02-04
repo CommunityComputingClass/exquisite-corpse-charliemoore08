a0064179bc55aa371dda9919c03b02fb12870ce5
charliemoore08 <163169264+charliemoore08@users.noreply.github.com>
2025-02-04 19:49:30 +0000

    Coffee Vs. Tea Survey

--- project/Survey.js
+++ project/Survey.js
@@ -1,37 +1,34 @@
-let myMessage = "Press the Up arrow if you drink coffee or the Down arrow if you drink tea";
+let myMessage = "Press the left key if you drink coffee or the right if you drink tea";
+
 let likes = 0; 
 let dislikes = 0; 
-let img;
+let bgImage;
 
-// Load the image and create a p5.Image object.
 function preload() {
-  img = loadImage('/assets/laDefense.jpg');
+  bgImage = loadImage('pic.jpg'); 
 }
 
 function setup() {
-  createCanvas(600, 600);
-
-  // Draw the image.
-  image(img, 0, 0);
-
-  describe('Image of the underside of a white umbrella and a gridded ceiling.');
+  createCanvas(850, 600);
+  noStroke();
 }
+
 function draw() { 
-  loadImage (img,0,0)
-  fill(255);
+  background(bgImage); // backround image
+  fill(255, 150);
   text(myMessage, 30, 50);
-  
+
   fill("brown");
-  stroke(255,255,255)
+  stroke(0);
+  strokeWeight(1.5);
   rect(40, 100, likes * 10, 30);
   
   fill("green");
-  stroke(255,255,255)
   rect(40, 200, dislikes * 10, 30);
   
   
-  fill(255);
-  textSize(16);
+  fill(250, 150);
+  textSize(27);
   text("Coffee Drinkers: " + likes, 50, 90);
   
   
@@ -39,10 +36,10 @@ function draw() {
 }
 
 function keyPressed(){
-  if(keyCode === UP_ARROW){
+  if(keyCode === LEFT_ARROW){
     likes = likes + 1;
   }  
-  if(keyCode === DOWN_ARROW){
+  if(keyCode === RIGHT_ARROW){
     dislikes = dislikes + 1;
   }    
 }
